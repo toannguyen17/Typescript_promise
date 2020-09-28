11ecdad2ee048505af012dcc28335490c1523fb4
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-09-28 09:33:32 +0700

    first commit

---
+++ src/functions.ts
@@ -0,0 +1,26 @@
+// import './scss/styles.scss';
+
+function square(num: number): number {
+  return num * num;
+}
+
+console.log(square(5));
+// Output: 25
+
+// Function expression
+const squareFE = function (num: number): number {
+  return num * num;
+}
+console.log(squareFE(9));
+
+// Higher-order function
+function add(a: number): Function {
+  return function(b: number): number {
+    return a + b;
+  }
+}
+
+const addWith5 = add(5);
+console.log(addWith5(3));
+console.log(addWith5(15));
+
